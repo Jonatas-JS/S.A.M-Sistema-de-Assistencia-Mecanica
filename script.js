38dfad6983562daafb36f77b538c7463d5df14c3
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2022-05-12 21:46:44 -0300

    modal em 80%
    O modal já está "criado" faltam ainda cadastrar as subcategorias.
    O botão de Novo Ticket já está abrindo o modal e o Cancelar fecha o modal.

---
+++ script.js
@@ -0,0 +1,6 @@
+function btnNewTicketClient() {
+  document.querySelector('.new-ticket-screen').classList.add('active')
+}
+function btnCancelarTicket() {
+  document.querySelector('.new-ticket-screen.active').classList.remove('active')
+}
